d0d53a0b67ec9c0cdf1c00e35a8dff447cb1c5db
Kevin Chandra <kacol.bot@gmail.com>
2022-05-15 15:04:58 +0700

    units cms

---
+++ app/Filament/Resources/UnitResource.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UnitResource\Pages;
+use App\Filament\Resources\UnitResource\RelationManagers;
+use App\Models\Unit;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class UnitResource extends Resource
+{
+    protected static ?string $navigationGroup = 'COGS';
+
+    protected static ?string $model = Unit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageUnits::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/UnitResource/Pages/ManageUnits.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\UnitResource\Pages;
+
+use App\Filament\Resources\UnitResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageUnits extends ManageRecords
+{
+    protected static string $resource = UnitResource::class;
+}
