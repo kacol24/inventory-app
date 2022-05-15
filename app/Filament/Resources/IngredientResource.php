ac022df8ab37967d6be47ac87013ca4345c09e4d
Kevin Chandra <kacol.bot@gmail.com>
2022-05-15 23:00:07 +0700

    ingredient resource

---
+++ app/Filament/Resources/IngredientResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\IngredientResource\Pages;
+use App\Filament\Resources\IngredientResource\RelationManagers;
+use App\Models\Ingredient;
+use App\Models\Unit;
+use Filament\Forms;
+use Filament\Forms\Components\BelongsToSelect;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class IngredientResource extends Resource
+{
+    protected static ?string $navigationGroup = 'COGS';
+
+    protected static ?string $model = Ingredient::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                BelongsToSelect::make('unit_id')
+                        ->relationship('unit', 'name')
+                        ->createOptionForm([
+                            Forms\Components\TextInput::make('name')->required(),
+                            Forms\Components\TextInput::make('symbol'),
+                        ])
+                        ->searchable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListIngredients::route('/'),
+            'create' => Pages\CreateIngredient::route('/create'),
+            'edit' => Pages\EditIngredient::route('/{record}/edit'),
+        ];
+    }
+}
